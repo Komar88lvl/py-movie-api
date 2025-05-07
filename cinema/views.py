e75fa29530f6e99811c73e6c38dd933c207e5c92
Sviatoslav <djsv91@gmail.com>
2025-05-07 15:55:43 +0300

    Add PUT & DELETE methods realization

--- cinema/views.py
+++ cinema/views.py
@@ -1,4 +1,4 @@
-from django.http import JsonResponse, HttpRequest
+from django.http import HttpRequest
 from rest_framework.decorators import api_view
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
@@ -8,17 +8,30 @@ from cinema.models import Movie
 from cinema.serializers import MovieSerializer
 
 
-@api_view(["GET"])
-def movie_list(request: HttpRequest) -> JsonResponse:
+@api_view(["GET", "POST"])
+def movie_list(request: HttpRequest) -> Response:
     if request.method == "GET":
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    else:
+        serializer = MovieSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(["GET"])
-def movie_detail(request: HttpRequest, pk: int):
+@api_view(["GET", "PUT", "DELETE"])
+def movie_detail(request: HttpRequest, pk: int) -> Response:
+    movie = get_object_or_404(Movie, pk=pk)
     if request.method == "GET":
-        movie = get_object_or_404(Movie, pk=pk)
         serializer = MovieSerializer(movie)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == "PUT":
+        serializer = MovieSerializer(movie, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    else:
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
